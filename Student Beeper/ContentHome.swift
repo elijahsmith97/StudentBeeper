3a5af453c59c2893396d4db4cef0ca271f7e3841
elijahsmith97 <smith.elijah97@gmail.com>
2020-02-04 23:47:18 -0500

    first commit

---
+++ Student Beeper/ContentHome.swift
@@ -0,0 +1,33 @@
+//
+//  ContentHome.swift
+//  Student Beeper
+//
+//  Created by Elijah Smith on 2/4/20.
+//  Copyright © 2020 Elijah Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentHome: View {
+    var body: some View {
+        HStack {
+            Image("wheel").resizable().frame(width: 80, height: 60)
+                .clipShape(Circle())
+            VStack(alignment: .leading) {
+                Text("Student Beeper")
+                .font(.largeTitle)
+                    .fontWeight(.semibold)
+                Text("Boone")
+                .foregroundColor(Color.blue)
+                
+            }
+            Spacer()
+        }.padding()
+    }
+}
+
+struct ContentHome_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentHome()
+    }
+}
---
+++ Student Beeper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Student Beeper
+//
+//  Created by Elijah Smith on 2/4/20.
+//  Copyright © 2020 Elijah Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
