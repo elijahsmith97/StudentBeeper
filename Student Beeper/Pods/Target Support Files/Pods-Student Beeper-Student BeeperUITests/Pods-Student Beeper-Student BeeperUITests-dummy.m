e73cbfaa0a2d86e5e8363bb0244971acfc382347
elijahsmith97 <smith.elijah97@gmail.com>
2020-02-24 14:13:59 -0500

    Podfile for Firebase implemented.

---
+++ Student Beeper/Pods/Target Support Files/Pods-Student Beeper-Student BeeperUITests/Pods-Student Beeper-Student BeeperUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Student_Beeper_Student_BeeperUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Student_Beeper_Student_BeeperUITests
+@end
