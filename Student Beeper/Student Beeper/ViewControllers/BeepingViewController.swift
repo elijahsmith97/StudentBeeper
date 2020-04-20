d9ed6fc66c97837fea614407a0c6076da4f5c8e1
elijahsmith97 <smith.elijah97@gmail.com>
2020-04-19 22:13:17 -0400

    set up new text fields for specifications on beeping

--- Student Beeper/Student Beeper/ViewControllers/BeepingViewController.swift
+++ Student Beeper/Student Beeper/ViewControllers/BeepingViewController.swift
@@ -12,39 +12,78 @@ import Firebase
 
 class BeepingViewController: UIViewController {
     
+    @IBOutlet weak var postButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    @IBOutlet weak var beepingPageLabel: UILabel!
+    
+    @IBOutlet weak var vehicleTypeTextField: UITextField!
+    
+    @IBOutlet weak var capacityTextField: UITextField!
+    
+    @IBOutlet weak var timeConstraintTextField: UITextField!
+    
+    @IBOutlet weak var successLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setUpElements()
         
     }
     
     func setUpElements() {
         errorLabel.alpha = 0
+        successLabel.alpha = 0
     }
     
     func validateFields() -> String? {
-        if beepingPosting.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
-            return "Please fill in the box"
+        let capacity = capacityTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let vehicle = vehicleTypeTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let timeConstraint = timeConstraintTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if capacity == "" || vehicle == "" || timeConstraint == "" {
+            return "Please fill in all the boxes"
         }
         
         return nil
     }
     
-    @IBOutlet weak var beepingPosting: UITextView!
-    
-    @IBAction func postButton(_ sender: Any) {
-    }
-    @IBOutlet weak var errorLabel: UILabel!
-    
     @IBAction func postTapped(_ sender: Any) {
         let error = validateFields()
         
-        let postDescription = beepingPosting.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        if error != nil {
+            showError(error!)
+        }
+        let capacity = capacityTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let vehicle = vehicleTypeTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let timeConstraint = timeConstraintTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
         
         let db = Firestore.firestore()
         db.collection("posts")
-            .addDocument(data: ["post_description":postDescription])
+            .addDocument(data: ["capacity":capacity,
+                                "vehicleType":vehicle,
+                                "timeConstraint":timeConstraint])
+        clearText()
+        showSuccess()
+                                    
     }
     
+    func showError(_ message:String) {
+        
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
+    func clearText() {
+        capacityTextField.text = ""
+        vehicleTypeTextField.text = ""
+        timeConstraintTextField.text = ""
+    }
+    
+    func showSuccess() {
+        successLabel.alpha = 1
+    }
     
 }
